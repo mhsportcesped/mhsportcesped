c6af5fd7ce206ac925cade6938da75bb6ee40559
Xtower44 <twitchxtower111@gmail.com>
2026-04-30 20:47:42 +0200

    Rollos usan mismas imágenes que al-corte

--- update_images.cjs
+++ update_images.cjs
@@ -16,18 +16,19 @@ const replacements = {
     '"danubio-15-corte"': { main: 'danubioMain', gallery: '[danubioMain, danubio1, danubio2, danubio3]' },
     '"viena-colores-corte"': { main: 'vienaMain', gallery: '[vienaMain, viena1, viena2, vienaTech]' },
     '"oslo-7-corte"': { main: 'osloMain', gallery: '[osloMain, osloTech]' },
-    
-    '"ankara-45-rollo"': { main: 'prodAnkaraRollo', gallery: '[prodAnkaraRollo, ankaraMain, ankara1, ankara2, ankara3, ankara4, ankara5, ankaraTech]' },
-    '"praga-40-rollo"': { main: 'prodPragaRollo', gallery: '[prodPragaRollo, pragaMain, praga1, praga2, praga3, pragaTech]' },
+
+    // Rollo: misma imagen principal que al corte, foto de rollo en galería
+    '"ankara-45-rollo"': { main: 'ankaraMain', gallery: '[ankaraMain, prodAnkaraRollo, ankara1, ankara2, ankara3, ankara4, ankara5, ankaraTech]' },
+    '"praga-40-rollo"': { main: 'pragaMain', gallery: '[pragaMain, prodPragaRollo, praga1, praga2, praga3, pragaTech]' },
     '"helsinki-30-rollo"': { main: 'helsinkiMain', gallery: '[helsinkiMain, helsinki1, helsinki2, helsinki3, helsinki4, helsinkiTech]' },
-    '"kiev-20-rollo"': { main: 'prodKievRollo', gallery: '[prodKievRollo, kievMain, kiev1, kiev2, kiev3, kiev4, kievTech]' },
-    '"oporto-33-rollo"': { main: 'prodOportoRollo', gallery: '[prodOportoRollo, oportoMain, oporto1, oporto2, oporto3, oportoTech]' },
-    '"berlin-24-rollo"': { main: 'prodBerlinRollo', gallery: '[prodBerlinRollo, berlinMain, berlin1, berlin2, berlin3, berlinTech]' },
-    '"budapest-18-rollo"': { main: 'prodBudapestRollo', gallery: '[prodBudapestRollo, budapestMain, budapest1, budapest2, budapest3, budapestTech]' },
-    '"danubio-15-rollo"': { main: 'prodDanubioRollo', gallery: '[prodDanubioRollo, danubioMain, danubio1, danubio2, danubio3]' },
-    '"viena-colores-rollo"': { main: 'prodVienaRollo', gallery: '[prodVienaRollo, vienaMain, viena1, viena2, vienaTech]' },
-    '"suecia-7-rollo"': { main: 'prodSueciaRollo', gallery: '[prodSueciaRollo, prodSueciaCorte]' },
-    '"oslo-7-rollo"': { main: 'prodOsloRollo', gallery: '[prodOsloRollo, osloMain, osloTech]' },
+    '"kiev-20-rollo"': { main: 'kievMain', gallery: '[kievMain, prodKievRollo, kiev1, kiev2, kiev3, kiev4, kievTech]' },
+    '"oporto-33-rollo"': { main: 'oportoMain', gallery: '[oportoMain, prodOportoRollo, oporto1, oporto2, oporto3, oportoTech]' },
+    '"berlin-24-rollo"': { main: 'berlinMain', gallery: '[berlinMain, prodBerlinRollo, berlin1, berlin2, berlin3, berlinTech]' },
+    '"budapest-18-rollo"': { main: 'budapestMain', gallery: '[budapestMain, prodBudapestRollo, budapest1, budapest2, budapest3, budapestTech]' },
+    '"danubio-15-rollo"': { main: 'danubioMain', gallery: '[danubioMain, prodDanubioRollo, danubio1, danubio2, danubio3]' },
+    '"viena-colores-rollo"': { main: 'vienaMain', gallery: '[vienaMain, prodVienaRollo, viena1, viena2, vienaTech]' },
+    '"suecia-7-rollo"': { main: 'prodSueciaCorte', gallery: '[prodSueciaCorte, prodSueciaRollo]' },
+    '"oslo-7-rollo"': { main: 'osloMain', gallery: '[osloMain, prodOsloRollo, osloTech]' },
 };
 
 let newLines = [];
@@ -39,16 +40,14 @@ for (let i = 0; i < lines.length; i++) {
             newLines.push(line);
             let j = i + 1;
             while (j < i + 15 && j < lines.length) {
-                if (lines[j].includes('image:')) {
+                if (lines[j].includes('image:') && !lines[j].includes('images:')) {
                     newLines.push(`    image: ${replacements[id].main},`);
                 } else if (lines[j].includes('images:')) {
                     newLines.push(`    images: ${replacements[id].gallery},`);
                 } else if (lines[j].includes('id:')) {
                     break;
                 } else {
-                    if (!lines[j].includes('image:') && !lines[j].includes('images:')) {
-                        newLines.push(lines[j]);
-                    }
+                    newLines.push(lines[j]);
                 }
                 j++;
             }
@@ -63,4 +62,4 @@ for (let i = 0; i < lines.length; i++) {
 }
 
 fs.writeFileSync(filePath, newLines.join('\n'), 'utf8');
-console.log('Done updating products.ts');
+console.log('Done! All rollo products now use the same main image as al-corte.');
